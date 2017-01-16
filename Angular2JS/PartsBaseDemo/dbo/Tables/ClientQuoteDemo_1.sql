8942e2eb8ec4149dc19fb59f38212e37e6259dd3
Juan Manuel Paredes <Juan Manuel Paredes@DESKTOP-DCMFDB9>
2017-01-16 12:50:49 -0600

    add schema DB

---
+++ Angular2JS/PartsBaseDemo/dbo/Tables/ClientQuoteDemo_1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[ClientQuoteDemo] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [CreatedBy]    NVARCHAR (250) NULL,
+    [DateCreated]  DATETIME       NULL,
+    [ModifiedBy]   NVARCHAR (250) NULL,
+    [DateModified] DATETIME       NULL,
+    [FirstName]    NVARCHAR (250) NULL,
+    [LastName]     NVARCHAR (250) NULL,
+    [Phone]        NVARCHAR (250) NULL,
+    [CountryId]    INT            NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Angular2JS/PartsBaseDemo/dbo/Tables/QuoteDemo_1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[QuoteDemo] (
+    [Id]            INT            IDENTITY (1, 1) NOT NULL,
+    [CreatedBy]     NVARCHAR (250) NULL,
+    [DateCreated]   DATETIME       NULL,
+    [ModifiedBy]    NVARCHAR (250) NULL,
+    [DateModified]  DATETIME       NULL,
+    [QuoteNumber]   BIGINT         NULL,
+    [ClientQuoteId] INT            NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_QuoteDemo_ClientQuoteId_ClientQuoteDemo_Id] FOREIGN KEY ([ClientQuoteId]) REFERENCES [dbo].[ClientQuoteDemo] ([Id])
+);
+
