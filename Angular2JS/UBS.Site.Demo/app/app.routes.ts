501ee9338d2babb21ccb1bec47a37700e218a5eb
rm_sa <rm_sa@DESKTOP-05U8E2S>
2017-01-17 13:15:45 -0600

    Added comments

--- Angular2JS/UBS.Site.Demo/app/app.routes.ts
+++ Angular2JS/UBS.Site.Demo/app/app.routes.ts
@@ -3,12 +3,12 @@ import { DataListComponent } from './data-list.component';
 
 // Route config let's you map routes to components
 const routes: Routes = [
-  // map '/allData' to the people list component
+  // map '/Home' to the people list component
   {
     path: 'Home',
     component: DataListComponent,
   },
-  // map '/' to '/allData' as our default route
+  // map '/' to '/Home' as our default route
   {
     path: '',
     redirectTo: '/Home',
